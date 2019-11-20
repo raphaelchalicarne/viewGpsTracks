7156531a9e5d39d94b8cafa6504d077c98e28f80
Raphaël Chalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2019-11-20 22:16:13 +0100

    Fastkml complicated + using elementTree

--- viewGpsFiles.py
+++ viewGpsFiles.py
@@ -11,6 +11,43 @@ import matplotlib.pyplot as plt
 import mplleaflet
 import os
 from fastkml import kml
+import xml.etree.ElementTree as ET
 
-dir = os.path.join('Centrale Lyon','Vélo','OruxMaps_2019-11-20 1801-Centrale-Maison')
-filename = os.path.join(dir,'20-11-19-Centrale-maison.kml')
+dir = os.path.join('Vélo','OruxMaps_2019-11-20 1801-Centrale-Maison')
+kml_file = os.path.join(dir,'20-11-19-Centrale-maison.kml')
+
+# %% Fast kml
+
+#Read file into string and convert to UTF-8 (Python3 style)
+with open(kml_file, 'rt') as myfile:
+    doc = myfile.read().encode('utf-8')
+    
+
+# Create the KML object to store the parsed result
+k = kml.KML()
+
+# Read in the KML string
+k.from_string(doc)
+
+# Check that the number of features is correct
+# This corresponds to the single ``Document``
+features = list(k.features())
+
+f2 = list(features[0].features())
+f3_0 = list(f2[0].features())
+
+# %% Using ElementTree
+
+tree = ET.parse(kml_file) 
+#root = tree.getroot()
+#tag_link = root.tag
+lineStrings = tree.findall('.//{http://earth.google.com/kml/2.2}LineString')
+
+for attributes in lineStrings:
+    for subAttribute in attributes:
+        if subAttribute.tag == '{http://earth.google.com/kml/2.2}coordinates':
+            print(subAttribute.tag, subAttribute.text)
+
+
+
+
