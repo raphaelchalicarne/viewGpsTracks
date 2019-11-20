d0418b910fd496446b1b759078004705a39f8959
Raphaël Chalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2019-11-20 23:57:10 +0100

    filtering of the 'coordinates' line strings

--- viewGpsFiles.py
+++ viewGpsFiles.py
@@ -8,13 +8,24 @@ Created on Wed Nov 20 19:08:42 2019
 # %% IMPORTS
 
 import matplotlib.pyplot as plt
+import numpy as np
 import mplleaflet
 import os
+import re
 from fastkml import kml
 import xml.etree.ElementTree as ET
 
 dir = os.path.join('Vélo','OruxMaps_2019-11-20 1801-Centrale-Maison')
 kml_file = os.path.join(dir,'20-11-19-Centrale-maison.kml')
+
+# %% Functions
+def llaf2array(llaf):
+    """Input : long_lat_alt_filtered
+    Output : numpy array with [long, lat, alt] at each line"""
+    splitted_array = [line.split(',') for line in llaf]
+    splitted_array = np.array(splitted_array, dtype=str)
+    array2float = splitted_array.astype(np.float)
+    return array2float
 
 # %% Fast kml
 
@@ -38,16 +49,25 @@ f3_0 = list(f2[0].features())
 
 # %% Using ElementTree
 
-tree = ET.parse(kml_file) 
-#root = tree.getroot()
-#tag_link = root.tag
-lineStrings = tree.findall('.//{http://earth.google.com/kml/2.2}LineString')
+tree = ET.parse(kml_file)
+
+root = tree.getroot()
+tag_link = root.tag
+# We delete every character that comes after '{' in the tag link.
+link = re.sub(r'}.*$', "", tag_link)
+
+lineStrings = tree.findall('.//' + link + '}LineString')
 
 for attributes in lineStrings:
     for subAttribute in attributes:
-        if subAttribute.tag == '{http://earth.google.com/kml/2.2}coordinates':
-            print(subAttribute.tag, subAttribute.text)
+        if subAttribute.tag == link + '}coordinates':
+            raw_long_lat_alt = subAttribute.text
 
-
+# We split the string into an array of lines
+long_lat_alt = raw_long_lat_alt.splitlines()
+# We remove the empty lines (containing no digits)
+long_lat_alt_filtered = [line for line in long_lat_alt if re.search(r'\d+',line)]
+# We convert it into an array where each line is [long, lat, alt].
+coordinates = llaf2array(long_lat_alt_filtered)
 
 
