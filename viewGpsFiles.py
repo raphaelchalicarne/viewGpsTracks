81cd9b58ff6382bddd596d96993ec6d72ceb0897
Raphaël <raphael.chalicarne@ecl17.ec-lyon.fr>
2019-11-21 13:22:56 +0100

    Refactoring + ajout gpx

--- viewGpsFiles.py
+++ viewGpsFiles.py
@@ -16,12 +16,14 @@ import gpxpy
 import gpxpy.gpx
 import xml.etree.ElementTree as ET
 
-dir = os.path.join('Vélo','OruxMaps_2019-11-18 1906-livraison-Stuart')
-filename = '18-11-19-livraison-Stuart.kml'
+#dir = os.path.join('Vélo','OruxMaps_2019-11-18 1906-livraison-Stuart')
+#filename = '18-11-19-livraison-Stuart.kml'
 #dir = os.path.join('Vélo','OruxMaps_2019-11-20 1801-Centrale-Maison')
 #filename = '20-11-19-Centrale-maison.kml'
+dir = os.path.join('Vélo','RaidECL_2018_J1')
+filename = 'EnsembleJ1.gpx'
 
-kml_file = os.path.join(dir,filename)
+file = os.path.join(dir,filename)
 
 # %% Functions
 
@@ -53,28 +55,45 @@ def llaf2array(llaf):
     array2float = splitted_array.astype(np.float)
     return array2float
 
-# %% Using ElementTree
+# %% Using ElementTree (kml files)
+if path_show_ext(file)[2] == '.kml':
+    tree = ET.parse(file)
+    
+    root = tree.getroot()
+    tag_link = root.tag
+    # We delete every character that comes after '{' in the tag link.
+    link = re.sub(r'}.*$', "", tag_link)
+    
+    lineStrings = tree.findall('.//' + link + '}LineString')
+    
+    for attributes in lineStrings:
+        for subAttribute in attributes:
+            if subAttribute.tag == link + '}coordinates':
+                raw_long_lat_alt = subAttribute.text
+    
+    # We split the string into an array of lines
+    long_lat_alt = raw_long_lat_alt.splitlines()
+    # We remove the empty lines (containing no digits)
+    long_lat_alt_filtered = [line for line in long_lat_alt if re.search(r'\d+',line)]
+    # We convert it into an array where each line is [long, lat, alt].
+    coordinates = llaf2array(long_lat_alt_filtered)
 
-tree = ET.parse(kml_file)
+# %% Using gpxpy (gpx files)
+if path_show_ext(file)[2] == '.gpx':
+    
+    gpx_file = open(file, 'r')
+    gpx = gpxpy.parse(gpx_file)
+    
+    coordinates = []
 
-root = tree.getroot()
-tag_link = root.tag
-# We delete every character that comes after '{' in the tag link.
-link = re.sub(r'}.*$', "", tag_link)
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                coordinates.append([point.longitude, point.latitude, point.elevation])
+    
+    coordinates = np.array(coordinates)
 
-lineStrings = tree.findall('.//' + link + '}LineString')
-
-for attributes in lineStrings:
-    for subAttribute in attributes:
-        if subAttribute.tag == link + '}coordinates':
-            raw_long_lat_alt = subAttribute.text
-
-# We split the string into an array of lines
-long_lat_alt = raw_long_lat_alt.splitlines()
-# We remove the empty lines (containing no digits)
-long_lat_alt_filtered = [line for line in long_lat_alt if re.search(r'\d+',line)]
-# We convert it into an array where each line is [long, lat, alt].
-coordinates = llaf2array(long_lat_alt_filtered)
+# %% Showing track on OSM with leaflet
 
 longitude = coordinates[:,0]
 latitude = coordinates[:,1]
@@ -82,5 +101,3 @@ altitude = coordinates[:,2]
 
 plt.plot(longitude, latitude, color = 'r', linewidth=4)
 mplleaflet.show()
-
-
